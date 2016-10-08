5ab44e91adf031f157b5c54b4e1c23ea0e9deec3
Erik DeBill <erik@debill.org>
2016-10-08 06:46:52 -0500

    horizontal lines all up the page, with margins

--- lib/writing_templates.ex
+++ lib/writing_templates.ex
@@ -1,31 +1,67 @@
 defmodule WritingTemplates do
   def go(%{horizontal_spacing: horizontal_spacing, horizontal_width: horizontal_width}) do
     {:ok, pid} = Gutenex.start_link
-    Gutenex.line_width(pid, horizontal_width)
-    |> horizontal_line(height(horizontal_spacing, 0))
-    |> horizontal_line(height(horizontal_spacing, 1))
-    |> horizontal_line(height(horizontal_spacing, 2))
-    |> horizontal_line(height(horizontal_spacing, 3))
-    |> horizontal_line(height(horizontal_spacing, 4))
-    |> horizontal_line(height(horizontal_spacing, 5))
-    |> horizontal_line(height(horizontal_spacing, 6))
-    |> Gutenex.export("./tmp/template.pdf")
+    output_filename = "./tmp/template.pdf"
+    
+    Gutenex.line_width(pid, line_width)
+    |> draw_horizontal_lines(horizontal_spacing)
+    |> Gutenex.export(output_filename)
     |> Gutenex.stop
+
+    System.cmd("open", [output_filename])
   end
 
   def go do
     go(%{horizontal_spacing: 9, horizontal_width: 0.5})
   end
 
+  def draw_horizontal_lines(doc, spacing) do
+    draw_horizontal_lines(doc, spacing, 0)
+  end
+
+  def draw_horizontal_lines(doc, spacing, n) do
+    horizontal_line(doc, height(spacing, n))
+
+    tm = top_margin
+    case height(spacing, n + 1) do
+      h when h > tm -> doc
+      _ -> draw_horizontal_lines(doc, spacing, n + 1)
+    end
+  end
+  
   def height(spacing, n) do  
-    n * (spacing / 25.4) * 72 # 72 pt/in 25.4 mm/in spacing mm / line
+    bottom_margin + n * (spacing / 25.4) * 72 # 72 pt/in 25.4 mm/in spacing mm / line
   end
 
   def horizontal_line(doc, height) do
-    Gutenex.line(doc, {{0, height}, {100, height}})
+    Gutenex.line(doc, {{left_margin, height}, {right_margin, height}})
+  end
+
+  def top_margin do
+    inches_to_pt(11) - inches_to_pt(1) # 1 in at top of page
+  end
+  
+  def bottom_margin do
+    inches_to_pt(0.75)
+  end
+
+  def right_margin do
+    inches_to_pt(8.5) - inches_to_pt(0.75)
+  end
+  
+  def left_margin do
+    inches_to_pt(0.75)
+  end
+
+  def inches_to_pt(inches) do
+    inches * 72.0
+  end
+
+  def mm_to_pt(mm) do
+    inches_to_pt(mm / 25.4)
   end
 
   def line_width do
-    0.3
+    0.25
   end
 end
