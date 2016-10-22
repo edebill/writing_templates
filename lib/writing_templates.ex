c13d7956679e6c41a586c1d3bbb4f45c05e2b7e3
Erik DeBill <erik@debill.org>
2016-10-22 09:32:44 -0500

    vertical lines across bottom of page

--- lib/writing_templates.ex
+++ lib/writing_templates.ex
@@ -3,8 +3,9 @@ defmodule WritingTemplates do
     {:ok, pid} = Gutenex.start_link
     output_filename = "./tmp/template.pdf"
     
-    Gutenex.line_width(pid, line_width)
+    Gutenex.line_width(pid, page.horizontal_line_width)
     |> draw_horizontal_lines(page)
+    |> draw_vertical_lines(page)
     |> Gutenex.export(output_filename)
     |> Gutenex.stop
 
@@ -20,37 +21,53 @@ defmodule WritingTemplates do
   end
 
   def draw_horizontal_lines(doc, page, n) do
-    horizontal_line(doc, page, height(page.horizontal_line_spacing, n))
+    horizontal_line(doc, page, height(page, n))
     spacing = page.horizontal_line_spacing
-    tm = top_margin
-    case height(spacing, n + 1) do
+    tm = page.top_margin
+    case height(page, n + 1) do
       h when h > tm -> doc
       _ -> draw_horizontal_lines(doc, page, n + 1)
     end
   end
+
+  def draw_vertical_lines(doc, page) do
+    Gutenex.line_width(doc, page.vertical_line_width)
+    |> draw_vertical_lines_across_bottom(page, 0)
+  end
   
-  def height(spacing, n) do  
-    bottom_margin + n * spacing
+  def draw_vertical_lines_across_bottom(doc, page, n) do
+    doc = draw_vertical_line(doc, page, origin_x(page, n), page.bottom_margin)
+
+    rm = page.right_margin
+    case origin_x(page, n + 1) do
+      h when h > rm -> doc
+      _ -> draw_vertical_lines_across_bottom(doc, page, n + 1)
+    end
+    
+  end
+
+  def origin_x(page, n) do
+    page.left_margin + n * page.vertical_line_spacing
+  end
+
+  def draw_vertical_line(doc, page, origin_x, origin_y) do
+    end_x = page.right_margin
+    end_y = (end_x - origin_x) * :math.tan((page.vertical_line_angle/360) * 2 * :math.pi) + page.bottom_margin
+
+    if end_y > page.top_margin do
+      end_y = page.top_margin
+      end_x = origin_x + (end_y - origin_y) / :math.tan((page.vertical_line_angle/360) * 2 * :math.pi)
+    end
+    
+    Gutenex.line(doc, {{origin_x, origin_y}, {end_x, end_y}})    
+  end
+
+  def height(page, n) do  
+    page.bottom_margin + n * page.horizontal_line_spacing
   end
 
   def horizontal_line(doc, page, height) do
     Gutenex.line(doc, {{page.left_margin, height}, {page.right_margin, height}})
-  end
-
-  def top_margin do
-    inches_to_pt(11) - inches_to_pt(1) # 1 in at top of page
-  end
-  
-  def bottom_margin do
-    inches_to_pt(0.75)
-  end
-
-  def right_margin do
-    inches_to_pt(8.5) - inches_to_pt(0.75)
-  end
-  
-  def left_margin do
-    inches_to_pt(0.75)
   end
 
   def inches_to_pt(inches) do
@@ -59,9 +76,5 @@ defmodule WritingTemplates do
 
   def mm_to_pt(mm) do
     inches_to_pt(mm / 25.4)
-  end
-
-  def line_width do
-    0.25
   end
 end
--- lib/writing_templates/page.ex
+++ lib/writing_templates/page.ex
@@ -1,8 +1,19 @@
 defmodule WritingTemplates.Page do
+  @doc """
+    All measurements are in pdf points (roughly 72/in).
+
+    Margins are relative to the origin in the lower left corner of the page.
+
+    Vertical line angle is measured from the horizontal in degrees. 45 would go from 
+    lower left to upper right. 52 was standard for Spencerian, 60-75 is modern.
+  """
   defstruct left_margin: 72,
     right_margin: 540,
     top_margin: 684,
     bottom_margin: 72,
     horizontal_line_width: 0.25,
-    horizontal_line_spacing: 25.5
+    horizontal_line_spacing: 25.5, # ~ 9mm/wide ruled
+    vertical_line_width: 0.1,
+    vertical_line_spacing: 36,
+    vertical_line_angle: 75
 end
