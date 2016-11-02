8bb46e9f1e94469aea040262537fb5c3ecf8f1e1
Erik DeBill <erik@debill.org>
2016-11-02 18:19:58 -0500

    a couple funcs for avoiding partial lines

--- lib/writing_templates/page.ex
+++ lib/writing_templates/page.ex
@@ -16,4 +16,27 @@ defmodule WritingTemplates.Page do
     vertical_line_width: 0.1,
     vertical_line_spacing: 36,
     vertical_line_angle: 60
+
+    
+  def trim_for_whole_lines(%WritingTemplates.Page{} = req) do
+    requested_height = lined_height(req)
+    num_lines = requested_height / req.horizontal_line_spacing
+    fractional = num_lines - trunc(num_lines)
+    
+    case fractional do
+      0 -> req
+      _ -> resize_and_center(req, trunc(num_lines) * req.horizontal_line_spacing)
+    end
+  end
+
+  def resize_and_center(orig, new_height) do
+    orig_height = lined_height(orig)
+    fudge = (orig_height - new_height) / 2
+    %WritingTemplates.Page{ orig | bottom_margin: orig.bottom_margin + fudge,
+                                   top_margin: orig.top_margin - fudge }
+  end
+
+  def lined_height(%WritingTemplates.Page{} = req) do
+    req.top_margin - req.bottom_margin
+  end
 end
